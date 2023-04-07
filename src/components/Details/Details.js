cc37c9d7aefba378724f4f043270b9b2ebc41c0e
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-07 15:37:54 +0600

    Added toastify

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './Details.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Details = ({details}) => {
-    console.log(details)
     let exerciseTime = 0;
     for(const detail of details){
         exerciseTime = exerciseTime + parseInt(detail.time);
     }
+    const addedToastify = () => {
+        toast('Congratulations ! Activity Completed')
+    }
     return (
         <div>
+            <ToastContainer></ToastContainer>
             <h4>Exercise Details</h4>
             <div className='exercise-detail'>
                 <div className='exercise-time'><p>Exercise Time</p><p className='set-time'>{exerciseTime}</p></div>
                 <div className='exercise-time'><p>Break Time</p><p className='set-time'>{exerciseTime}</p></div>
             </div>
-            <button className='add-to-list'>Activity Completed</button>
+            <button onClick={addedToastify} className='add-to-list'>Activity Completed</button>
         </div>
     );
 };
